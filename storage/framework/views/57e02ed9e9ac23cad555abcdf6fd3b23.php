3d44f5d6c70cca063090bc739889aa7e04ed0304
thanhsvaisn <133182915+thanhsvaisn@users.noreply.github.com>
2024-04-13 10:20:03 +0700

    Add files via upload

---
+++ storage/framework/views/57e02ed9e9ac23cad555abcdf6fd3b23.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</head>
+<body>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">bookid</th>
+            <th scope="col">authorid</th>
+            <th scope="col">title</th>
+            <th scope="col">ISBN</th>
+            <th scope="col">pub_year</th>
+            <th scope="col">available</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"></th>
+            <td><?php$table->bookid?></td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+</body>
+</html>
+<?php /**PATH D:\workspace\PHP\book-app\resources\views/welcome.blade.php ENDPATH**/ ?>
